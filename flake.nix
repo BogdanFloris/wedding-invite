686588c415096e458cd9e0ca51d6fe5ce0ed7885
Bogdan Floris <bogdan.floris@gmail.com>
2025-04-10 21:56:07 +0300

    feat: flake config

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Wedding invite";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+  inputs.templ.url = "github:a-h/templ";
+
+  outputs = { nixpkgs, flake-utils, templ, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        templOverlay = system: templ.packages.${system}.templ;
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.libcap pkgs.go pkgs.gcc pkgs.flyctl ];
+          buildInputs = [ (templOverlay system) ];
+        };
+      });
+}
--- go.mod
+++ go.mod
@@ -2,11 +2,8 @@ module wedding-invite
 
 go 1.24.1
 
-require github.com/mattn/go-sqlite3 v1.14.24
-
-require github.com/a-h/templ v0.3.833
-
 require (
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+	github.com/a-h/templ v0.3.857
+	github.com/joho/godotenv v1.5.1
+	github.com/mattn/go-sqlite3 v1.14.24
 )
